6a0a1148e52a2371ceb36b9602a5a9c56cc59e3d
JustBecerra <justj.becerra@gmail.com>
2023-10-27 14:18:43 -0300

    technologies screen done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { Information } from "../components/Information";
-import Image from "next/image";
-import nightRoad from "../../public/night-road.jpg";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
--- src/misc/index.tsx
+++ src/misc/index.tsx
@@ -1,1 +1,63 @@
+import reactlogo from "../../public/reactlogo.png";
+import reduxlogo from "../../public/reduxlogo.png";
+import typescriptlogo from "../../public/typescriptlogo.png";
+import javascriptlogo from "../../public/javascriptlogo.png";
+import nodelogo from "../../public/nodelogo.png";
+import mongologo from "../../public/mongologo.png";
+import nextlogo from "../../public/nextlogo.png";
+import expresslogo from "../../public/expresslogo.png";
+import tailwindlogo from "../../public/tailwindlogo.png";
+import MUIlogo from "../../public/MUIlogo.png";
+import postgreslogo from "../../public/postgreslogo.png";
 export const ButtonNames = ["Home", "Technologies", "Projects"];
+
+export const TechnologiesList = [
+  {
+    name: "Typescript",
+    icon: typescriptlogo,
+  },
+  {
+    name: "JavaScript",
+    icon: javascriptlogo,
+  },
+  {
+    name: "React",
+    icon: reactlogo,
+  },
+  {
+    name: "React Native",
+    icon: reactlogo,
+  },
+  {
+    name: "Redux",
+    icon: reduxlogo,
+  },
+  {
+    name: "Node",
+    icon: nodelogo,
+  },
+  {
+    name: "Next",
+    icon: nextlogo,
+  },
+  {
+    name: "Express",
+    icon: expresslogo,
+  },
+  {
+    name: "MongoDB",
+    icon: mongologo,
+  },
+  {
+    name: "Postgres SQL",
+    icon: postgreslogo,
+  },
+  {
+    name: "Tailwind",
+    icon: tailwindlogo,
+  },
+  {
+    name: "MUI",
+    icon: MUIlogo,
+  },
+];
