d00490400a60ac0ecaa9c3ab7e8d01442e0831f8
JustBecerra <justj.becerra@gmail.com>
2023-10-29 19:46:33 -0300

    add projects info and links

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,37 @@
 import { ProjectCard } from "@/components/ProjectCard";
 import { Box } from "@mui/material";
+import huellitasSH from "../../../public/huellitasSH.png";
+import caloriecounterSH from "../../../public/caloriecounterSH.png";
 import React from "react";
 
 export default function projects() {
+  const huellitasURL = "https://huellitas.vercel.app/home";
+  const huellitasGithubURL = "https://github.com/cdwenz/huellitas-pg";
+  const calorieCounterURL = "https://calorie-counter-xi.vercel.app";
+  const calorieCounterGitHubURL =
+    "https://github.com/JustBecerra/calorie-counter";
+  const cryptoTrackerGithubURL =
+    "https://github.com/JustBecerra/crypto-tracker";
   return (
     <main className="flex flex-col items-center justify-center">
-      <Box className="w-[80%] mt-[3%] flex flex-row justify-around">
-        <ProjectCard title={"Huellitas"} />
-        <ProjectCard title={"Crypto Tracker"} />
-        <ProjectCard title={"Calorie Counter"} />
+      <Box className="w-[80%] mt-[3%] flex flex-row justify-center items-center gap-[1rem]">
+        <ProjectCard
+          title={"Huellitas"}
+          screenshot={huellitasSH}
+          url={huellitasURL}
+          github={huellitasGithubURL}
+        />
+        <ProjectCard
+          title={"Crypto Tracker"}
+          screenshot={caloriecounterSH}
+          github={cryptoTrackerGithubURL}
+        />
+        <ProjectCard
+          title={"Calorie Counter"}
+          screenshot={caloriecounterSH}
+          url={calorieCounterURL}
+          github={calorieCounterGitHubURL}
+        />
       </Box>
     </main>
   );
--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,16 +1,38 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import React from "react";
-
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 type props = {
   title: string;
+  screenshot: StaticImageData;
+  url?: string;
+  github: string;
 };
 
-export const ProjectCard = ({ title }: props) => {
+export const ProjectCard = ({ title, screenshot, url, github }: props) => {
   return (
     <Card variant="outlined">
       <CardContent>
+        <Image src={screenshot} alt="" />
         <Typography>{title}</Typography>
+        <Typography>description</Typography>
       </CardContent>
+      <CardActions>
+        {url && (
+          <Link href={url} target="_blank">
+            <Button>Link</Button>
+          </Link>
+        )}
+        <Link href={github} target="_blank">
+          <Button>Github</Button>
+        </Link>
+      </CardActions>
     </Card>
   );
 };
