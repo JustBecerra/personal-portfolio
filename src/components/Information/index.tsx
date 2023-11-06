f44f7764efbf40c90ae7c86573ee48a01ca8852a
JustBecerra <justj.becerra@gmail.com>
2023-11-06 12:05:20 -0300

    add burger menu for mobile and make home responsive

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -10,21 +10,21 @@ import { useTranslations } from "next-intl";
 export const Information = () => {
   const t = useTranslations("home");
   return (
-    <Box className="flex w-[85%] h-auto flex-row justify-center items-center mt-[5%] gap-[10%]">
+    <Box className="flex w-[85%] h-auto lg:flex-row min-[310px]:flex-col sm:flex-col justify-center items-center mt-[5%] gap-[10%] min-[310px]:mt-[4rem]">
       <Box className="w-[40%] h-[40%]">
         <Image src={sonriente} alt="" style={{ borderRadius: "1rem" }} />
       </Box>
-      <Box className="flex justify-center items-center w-[40%] h-[50%] flex-col gap-[1rem]">
-        <Typography className="text-[2.5rem] font-medium text-center">
+      <Box className="flex justify-center items-center md:w-[40%] h-[50%] min-[310px]:w-[80%] flex-col gap-[1rem] min-[310px]:gap-[2rem]">
+        <Typography className="md:text-[2.5rem] min-[310px]:text-[1.25rem] md:whitespace-normal min-[310px]:whitespace-nowrap font-medium text-center min-[310px]:mt-[1rem]">
           {t("welcome")}
         </Typography>
-        <Typography className="text-[1.25rem] text-center">
+        <Typography className="md:text-[1.25rem] min-[310px]:text-[0.75rem] min-[310px]:w-[100%] text-center">
           {t("intro")}
         </Typography>
-        <Typography className="text-[1.25rem] text-center">
+        <Typography className="md:text-[1.25rem] min-[310px]:text-[0.75rem] min-[310px]:w-[100%] text-center">
           {t("ifyouwish")}
         </Typography>
-        <Box className="flex flex-row items-center gap-[2rem]">
+        <Box className="flex flex-row items-center gap-[2rem] max-[600px]:w-[100%]">
           <Link href="https://github.com/JustBecerra" target="_blank">
             <GitHubIcon className="w-[2rem] h-[2rem] rounded-full shadow-lg hover:shadow-white" />
           </Link>
@@ -34,7 +34,7 @@ export const Information = () => {
           >
             <LinkedInIcon className="w-[2rem] h-[2rem] rounded-lg shadow-lg hover:shadow-cyan-500" />
           </Link>
-          <Typography className="text-[1.25rem] font-medium">
+          <Typography className="text-[1.25rem] min-[310px]:text-[0.75rem] font-medium">
             justj.becerra@gmail.com
           </Typography>
         </Box>
--- src/components/navigation/Navbar/index.tsx
+++ src/components/navigation/Navbar/index.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import {
   Box,
   Button,
   FormControl,
+  IconButton,
   InputLabel,
   MenuItem,
   Select,
@@ -17,9 +18,27 @@ import spanishflag from "../../../../public/spanishflag.png";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 import { usePathname, useRouter } from "next-intl/client";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const Navbar = () => {
   const [language, setLanguage] = React.useState("en");
+  const [windowWidth, setWindowWidth] = React.useState(0);
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const t = useTranslations("navbar");
   const router = useRouter();
   const pathname = usePathname();
@@ -31,55 +50,63 @@ export const Navbar = () => {
   return (
     <AppBar position="static" className="bg-transparent">
       <Toolbar disableGutters>
-        <Box className="ml-[3%] w-[100%] flex flex-row gap-x-2 items-center">
-          <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-            <InputLabel
-              sx={{
-                "&.Mui-focused": {
+        {windowWidth > 620 ? (
+          <Box className="ml-[3%] w-[100%] flex flex-row gap-x-2 items-center">
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel
+                sx={{
+                  "&.Mui-focused": {
+                    color: "white",
+                  },
+                }}
+                className="text-white focus:text-white"
+              >
+                Language
+              </InputLabel>
+              <Select
+                onChange={handleChange}
+                value={language}
+                label="Language"
+                sx={{
+                  "&& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "white",
+                  },
                   color: "white",
-                },
-              }}
-              className="text-white focus:text-white"
-            >
-              Language
-            </InputLabel>
-            <Select
-              onChange={handleChange}
-              value={language}
-              label="Language"
-              sx={{
-                "&& .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "white",
-                },
-                color: "white",
-              }}
-            >
-              <MenuItem value="en">English</MenuItem>
-              <MenuItem value="es">Español</MenuItem>
-            </Select>
-          </FormControl>
-          <Image
-            src={language === "en" ? britishflag : spanishflag}
-            alt=""
-            width={38}
-            height={30}
-          />
-          <Link href="/">
-            <Button className="text-white normal-case">
-              {t("homenavbar")}
-            </Button>
-          </Link>
-          <Link href="/technologies">
-            <Button className="text-white normal-case">
-              {t("technologiesnavbar")}
-            </Button>
-          </Link>
-          <Link href="/projects">
-            <Button className="text-white normal-case">
-              {t("projectsnavbar")}
-            </Button>
-          </Link>
-        </Box>
+                }}
+              >
+                <MenuItem value="en">English</MenuItem>
+                <MenuItem value="es">Español</MenuItem>
+              </Select>
+            </FormControl>
+            <Image
+              src={language === "en" ? britishflag : spanishflag}
+              alt=""
+              width={38}
+              height={30}
+            />
+            <Link href="/">
+              <Button className="text-white normal-case">
+                {t("homenavbar")}
+              </Button>
+            </Link>
+            <Link href="/technologies">
+              <Button className="text-white normal-case">
+                {t("technologiesnavbar")}
+              </Button>
+            </Link>
+            <Link href="/projects">
+              <Button className="text-white normal-case">
+                {t("projectsnavbar")}
+              </Button>
+            </Link>
+          </Box>
+        ) : (
+          isMounted && (
+            <IconButton>
+              <MenuIcon />
+            </IconButton>
+          )
+        )}
       </Toolbar>
     </AppBar>
   );
