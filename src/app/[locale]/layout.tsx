912d026e519b910a7c899ca125fc4b7e53c51067
JustBecerra <justj.becerra@gmail.com>
2023-11-09 12:23:23 -0300

    add favicon and important for logo sizes

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,11 +4,13 @@ import "./globals.css";
 import { Navbar } from "../../components/navigation/Navbar";
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
+import favicon from "../favicon.ico";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: `JJB's Portfolio`,
   description: "Awesome Developer",
+  icons: [{ rel: "icon", url: favicon.src }],
 };
 
 export function generateStaticParams() {
@@ -31,6 +33,9 @@ export default async function RootLayout({
   }
   return (
     <html lang={locale}>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${inter.className} bg-[url('../../../public/night-road.jpg')] bg-no-repeat bg-cover min-h-screen h-auto w-screen `}
       >
--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -26,13 +26,13 @@ export const Information = () => {
         </Typography>
         <Box className="flex flex-row items-center gap-[2rem] max-[600px]:w-[100%]">
           <Link href="https://github.com/JustBecerra" target="_blank">
-            <GitHubIcon className="w-[2rem] h-[2rem] rounded-full shadow-lg hover:shadow-white" />
+            <GitHubIcon className="!w-[2rem] !h-[2rem] rounded-full shadow-lg hover:shadow-white" />
           </Link>
           <Link
             href="https://www.linkedin.com/in/justo-becerra-14868a1b4/"
             target="_blank"
           >
-            <LinkedInIcon className="w-[2rem] h-[2rem] rounded-lg shadow-lg hover:shadow-cyan-500" />
+            <LinkedInIcon className="!w-[2rem] !h-[2rem] rounded-lg shadow-lg hover:shadow-cyan-500" />
           </Link>
           <Typography className="md:!text-[1.5rem] min-[310px]:!text-[0.75rem] !font-medium">
             justj.becerra@gmail.com
